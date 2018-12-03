e185d46bf59d06d3b65a658d014320857f0857a3
夏天 <814940657@qq.com>
2018-12-03 21:26:12 +0800

    添加项目文件。

---
+++ 阿里妈妈导单/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 阿里妈妈导单
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
